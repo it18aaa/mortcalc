db682b39df4f60e1471e0d4579fbf2865fed8b8e
Ian Thomson <it18aaa@herts.ac.uk>
2020-03-12 12:17:51 +0000

    added user interface

--- demo.py
+++ demo.py
@@ -1,17 +1,56 @@
 #!/bin/env python
 from mort import *
 
+import matplotlib.pyplot as plt
 
 mort_years = 20
 principal = 125000
-int_rate = 1.8
+int_rate = 1.94
 
 # calc the repayment amount
 repayment = calcRepayment(12 * mort_years, principal, int_rate)
 
 graphData = getGraphData(12 * mort_years, principal, int_rate)
 
+owed = list()
+running_total_int = list()
+running_total_cap = list()
+running_total_all = list()
+owed.append(principal)
+running_total_cap.append(0)
+running_total_int.append(0)
+running_total_all.append(0)
+
+totint = 0
+totcap = 0
+
 for month in graphData:
-    print("{0} £{1} £{2} £{3}".format(month.get("number"), month.get("val"), month.get("cap"), month.get("int")))
+
+    print("{0} £{1} £{2} £{3}".format(month.get("number"), month.get("val"), month.get("cap"), month.get("int")))    
+    
+    print("total interest: £{0}  total capital: £{1}".format(totint, totcap))
+
+    totint += month.get("int")
+    totcap += month.get("cap")
+    #if int(month.get("number")) % 12 == 0:
+    owed.append(month.get("val"))
+    running_total_cap.append(totcap)
+    running_total_int.append(totint)
+    running_total_all.append(totcap + totint)
 
 print("repayment "  + str(repayment))
+
+plt.figure()
+plt.title('mortgage calc')
+plt.plot(owed, 'r-')
+plt.axis([0,300,0, principal*2])
+plt.ylabel('amount')
+plt.xlabel('month')
+plt.plot(running_total_int, 'b')
+plt.plot(running_total_cap, 'g')
+plt.plot(running_total_all, 'y--')
+plt.grid(True, which='both')
+plt.minorticks_on()
+
+
+plt.show()
---
+++ main.py
@@ -0,0 +1,42 @@
+#!/bin/env python
+
+from mort import *
+from PyQt5 import QtWidgets, uic
+import sys
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        # Call the inherited classes __init__ method
+        super(Ui, self).__init__()
+        uic.loadUi('mort.ui', self)  # Load the .ui file
+        self.show()  # Show the GUI
+
+        # collect all the Ui pointers we need to interact with GUI
+        self.btnCalc = self.findChild(QtWidgets.QPushButton, 'btnCalc')
+        self.principal = self.findChild(QtWidgets.QSpinBox, 'principal')
+        self.interest = self.findChild(QtWidgets.QDoubleSpinBox, 'interest')
+        self.years = self.findChild(QtWidgets.QSpinBox, 'years')
+        self.textOutput = self.findChild(QtWidgets.QTextEdit, 'output')
+
+        # connect GUI elements to functions
+        self.btnCalc.clicked.connect(self.calculate)
+
+    def calculate(self):
+        interest = self.interest.value()
+        principal = self.principal.value()
+        years = self.years.value()
+
+        self.textOutput.clear()
+        self.textOutput.append("Interest Rate: {0:.2f}%".format(interest))
+        self.textOutput.append("Principal: £" + str(principal))
+        self.textOutput.append("Years: " + str(years))
+
+        repayment = calcRepayment(12 * years, principal, interest)
+        graphData = getGraphData(12 * years, principal, interest)
+
+        self.textOutput.append("Repyament: £" + str(repayment) + " per month")
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
